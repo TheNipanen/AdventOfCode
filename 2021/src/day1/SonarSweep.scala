45ff63e0d5fe36b6224a43fa2013de94fc202d4d
Niilo Lappi <niilo.lappi@aalto.fi>
2021-12-02 12:04:27 +0200

    2021 day 1

---
+++ 2021/src/day1/SonarSweep.scala
@@ -0,0 +1,34 @@
+package day1
+
+import scala.io.Source
+
+object SonarSweep extends App {
+  val s = Source.fromFile("src/day1/input.txt")
+  val reader = s.bufferedReader()
+  
+  var line = reader.readLine()
+  var increaseCount = 0
+  while (line != null) {
+    val nextLine = reader.readLine()
+    if (nextLine != null && line.toInt < nextLine.toInt) {
+      increaseCount += 1
+    }
+    line = nextLine
+  }
+  println("Measurement increases: " + increaseCount)
+  
+  val ss = Source.fromFile("src/day1/input.txt")
+  val measurements = ss.getLines().toArray.map( _.toInt )
+  var i = 3
+  var prevSum = measurements(0) + measurements(1) + measurements(2)
+  increaseCount = 0
+  while (i < measurements.size) {
+    val nextSum = prevSum - measurements(i-3) + measurements(i)
+    if (prevSum < nextSum) {
+      increaseCount += 1
+    }
+    prevSum = nextSum
+    i += 1
+  }
+  println("Three-measeurement increases: " + increaseCount)
+}
